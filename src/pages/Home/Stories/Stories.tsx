cd98497947dc8cb8178ea11f56886884f5458e35
volp99 <stefanovolpatti@yahoo.it>
2021-11-20 21:12:05 +0100

    move data to a TS file

--- src/pages/Home/Stories/Stories.tsx
+++ src/pages/Home/Stories/Stories.tsx
@@ -1,48 +1,16 @@
 import React from "react";
 import style from "./Stories.module.sass";
-
-const INFO_DATA = [
-  {
-    key: 1,
-    img: "https://images2.corriereobjects.it/methode_image/2020/07/17/Tecnologia/Foto%20Tecnologia%20-%20Trattate/memoji_facecovering_seam_1-ki0--1600x1600@Corriere-Web-Sezioni_BC.png?v=202007200933",
-    alt: "emoji1",
-    username: "Mike_1",
-  },
-  {
-    key: 2,
-    img: "https://images2.corriereobjects.it/methode_image/2020/07/17/Tecnologia/Foto%20Tecnologia%20-%20Trattate/memoji_fauxhawk-ki0--1600x1600@Corriere-Web-Sezioni_BC.png?v=202007200933",
-    alt: "emoji2",
-    username: "Ste_vee",
-  },
-  {
-    key: 3,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYu1zfq0lUDk2aRbVpEFoO_jBQoNe_Xl_aslU-MAhhGBON_4zVRKF7tkYJdc8_7SPsrro&usqp=CAU",
-    alt: "emoji3",
-    username: "Robby",
-  },
-  {
-    key: 4,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYu1zfq0lUDk2aRbVpEFoO_jBQoNe_Xl_aslU-MAhhGBON_4zVRKF7tkYJdc8_7SPsrro&usqp=CAU",
-    alt: "emoji4",
-    username: "Bobdwdw",
-  },
-  {
-    key: 5,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRTxawrAzxPJJ6vnQ79hLnCaVwEa5tjkqvSA&usqp=CAU",
-    alt: "emoji5",
-    username: "Geezy_son",
-  },
-];
+import USER_DATA from "./story";
 
 const Stories: React.FC = () => {
   return (
     <>
       <div className={style["story-container"]}>
-        {INFO_DATA.map((data) => (
-          <div key={data.key}>
-            <img src={data.img} alt={data.alt} />
+        {USER_DATA.map((user) => (
+          <div key={user.key}>
+            <img src={user.img} alt={user.alt} />
             <div className={style.user}>
-              <span>{data.username}</span>
+              <span>{user.username}</span>
             </div>
           </div>
         ))}
---
+++ src/pages/Home/Stories/story.ts
@@ -0,0 +1,38 @@
+const USER_DATA: {
+  img: string;
+  alt: string;
+  key: number;
+  username: string;
+}[] = [
+  {
+    key: 1,
+    img: "https://images2.corriereobjects.it/methode_image/2020/07/17/Tecnologia/Foto%20Tecnologia%20-%20Trattate/memoji_facecovering_seam_1-ki0--1600x1600@Corriere-Web-Sezioni_BC.png?v=202007200933",
+    alt: "emoji1",
+    username: "Mike_1",
+  },
+  {
+    key: 2,
+    img: "https://images2.corriereobjects.it/methode_image/2020/07/17/Tecnologia/Foto%20Tecnologia%20-%20Trattate/memoji_fauxhawk-ki0--1600x1600@Corriere-Web-Sezioni_BC.png?v=202007200933",
+    alt: "emoji2",
+    username: "Ste_vee",
+  },
+  {
+    key: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYu1zfq0lUDk2aRbVpEFoO_jBQoNe_Xl_aslU-MAhhGBON_4zVRKF7tkYJdc8_7SPsrro&usqp=CAU",
+    alt: "emoji3",
+    username: "Robby",
+  },
+  {
+    key: 4,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYu1zfq0lUDk2aRbVpEFoO_jBQoNe_Xl_aslU-MAhhGBON_4zVRKF7tkYJdc8_7SPsrro&usqp=CAU",
+    alt: "emoji4",
+    username: "Bobdwdw",
+  },
+  {
+    key: 5,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRTxawrAzxPJJ6vnQ79hLnCaVwEa5tjkqvSA&usqp=CAU",
+    alt: "emoji5",
+    username: "Geezy_son",
+  },
+];
+export default USER_DATA;
