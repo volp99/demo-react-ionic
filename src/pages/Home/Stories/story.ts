2ba75b15b2a5024ce9a54af8c6c97dfd9d671d52
volp99 <stefanovolpatti@yahoo.it>
2021-12-01 22:44:05 +0100

    remove css in Real and add sass extension

--- src/pages/Home/Stories/story.ts
+++ src/pages/Home/Stories/story.ts
@@ -1,38 +1,55 @@
 const USER_DATA: {
   img: string;
+  post: string;
   alt: string;
   key: number;
   username: string;
+  comment: string;
 }[] = [
   {
     key: 1,
     img: "https://images2.corriereobjects.it/methode_image/2020/07/17/Tecnologia/Foto%20Tecnologia%20-%20Trattate/memoji_facecovering_seam_1-ki0--1600x1600@Corriere-Web-Sezioni_BC.png?v=202007200933",
+    post: "https://images.unsplash.com/photo-1514908944882-08f7f242beb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRpYW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
     alt: "emoji1",
     username: "Mike_1",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
   },
   {
     key: 2,
     img: "https://images2.corriereobjects.it/methode_image/2020/07/17/Tecnologia/Foto%20Tecnologia%20-%20Trattate/memoji_fauxhawk-ki0--1600x1600@Corriere-Web-Sezioni_BC.png?v=202007200933",
+    post: "https://images.unsplash.com/photo-1638366272238-ed18adaf9560?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
     alt: "emoji2",
     username: "Ste_vee",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
   },
   {
     key: 3,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYu1zfq0lUDk2aRbVpEFoO_jBQoNe_Xl_aslU-MAhhGBON_4zVRKF7tkYJdc8_7SPsrro&usqp=CAU",
+    post: "https://images.unsplash.com/photo-1638369321631-25b24283fcdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
     alt: "emoji3",
     username: "Robby",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
   },
   {
     key: 4,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYu1zfq0lUDk2aRbVpEFoO_jBQoNe_Xl_aslU-MAhhGBON_4zVRKF7tkYJdc8_7SPsrro&usqp=CAU",
+    post: "https://images.unsplash.com/photo-1638298927715-2e41b388ce0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
     alt: "emoji4",
     username: "Bobdwdw",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
   },
   {
     key: 5,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRTxawrAzxPJJ6vnQ79hLnCaVwEa5tjkqvSA&usqp=CAU",
+    post: "https://images.unsplash.com/photo-1638358473474-336230b4ccac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
     alt: "emoji5",
     username: "Geezy_son",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
   },
 ];
 export default USER_DATA;
